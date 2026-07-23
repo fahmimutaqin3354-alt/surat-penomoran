4a0bc9df7ccf59baa4cbaf5da1b7fbd99b87bf02
Fahmi <fahmimutaqin3354@gmail.com>
2026-07-23 11:34:04 +0700

    Membuat backend surat masuk dan surat keluar

--- app/Http/Controllers/SuratMasukController.php
+++ app/Http/Controllers/SuratMasukController.php
@@ -11,55 +11,61 @@ class SuratMasukController extends Controller
      * Display a listing of the resource.
      */
     public function index()
-    {
-        //
-    }
+{
+    $surat = SuratMasuk::all();
+
+    return view('surat_masuk.index', compact('surat'));
+}
 
     /**
      * Show the form for creating a new resource.
      */
     public function create()
-    {
-        //
-    }
+{
+    return view('surat_masuk.create');
+}
 
     /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
-    {
-        //
-    }
+{
+    SuratMasuk::create($request->all());
+
+    return redirect()->route('surat-masuk.index');
+}
 
     /**
      * Display the specified resource.
      */
     public function show(SuratMasuk $suratMasuk)
-    {
-        //
-    }
-
+{
+    return view('surat_masuk.show', compact('suratMasuk'));
+}
     /**
      * Show the form for editing the specified resource.
      */
     public function edit(SuratMasuk $suratMasuk)
-    {
-        //
-    }
+{
+    return view('surat_masuk.edit', compact('suratMasuk'));
+}
 
     /**
      * Update the specified resource in storage.
      */
     public function update(Request $request, SuratMasuk $suratMasuk)
-    {
-        //
-    }
+{
+    $suratMasuk->update($request->all());
 
+    return redirect()->route('surat-masuk.index');
+}
     /**
      * Remove the specified resource from storage.
      */
     public function destroy(SuratMasuk $suratMasuk)
-    {
-        //
-    }
+{
+    $suratMasuk->delete();
+
+    return redirect()->route('surat-masuk.index');
+}
 }
